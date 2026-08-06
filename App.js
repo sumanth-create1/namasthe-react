9336e808d35cb93729a24414a93653c0baf9e6d4
sumanth-create1 <sajjasatyasumanth@gmail.com>
2026-08-06 21:18:52 +0530

    that's about react core

--- App.js
+++ App.js
@@ -1,17 +1,43 @@
 /* <div id = "parent">
-    <div id = "child">
+    <div id = "child1">
+        <h1>I am an h1 tag </h1>
+    </div>
+
+    <div id = "child2">
         <h1>I am an h1 tag </h1>
     </div>
 </div> */
 
-
-
-const parent = React.createElement("div" , {id:"parent"}, 
-    React.createElement("div", {id:"child"}, 
-       [ React.createElement("h1", {} , "I am an H1 tag"),
-         React.createElement("h1", {} , "this i an another h1 (sibling of upper h1)")]
-    )
-);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "div",
+    { id: "child1" },
+    [React.createElement("h1", {}, "I am an H1 tag"),
+    React.createElement("h2", {}, "I am an H1 tag"),
+    React.createElement("h3", {}, "I am an H1 tag")]
+  ),
+  React.createElement(
+    "div",
+    { id: "child2" },
+    React.createElement("h2", {}, [React.createElement("h1", {}, "I am an H1 tag"),
+    React.createElement("h2", {}, "I am an H2 tag"),
+    React.createElement("h3", {}, "I am an H3 tag")]),
+  ),
+  React.createElement(
+    "div",
+    { id: "child2" },
+    React.createElement("h3", {}, [React.createElement("h1", {}, "I am an H1 tag"),
+    React.createElement("h2", {}, "I am an H2 tag"),
+    React.createElement("h3", {}, "I am an H3 tag")]),
+  ),
+  React.createElement(
+    "div",
+    { id: "child2" },
+    React.createElement("h4", {}, [React.createElement("h1", {}, "I am an H1 tag"),
+    React.createElement("h2", {}, "I am an H2 tag"),
+    React.createElement("h3", {}, "I am an H3 tag")]),
+  ),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
