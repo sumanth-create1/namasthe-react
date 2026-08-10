eafd85a188afc7745dc17a0cbc64db1cf298ecaa
sumanth-create1 <sajjasatyasumanth@gmail.com>
2026-08-10 23:45:23 +0530

    random commit

--- App.js
+++ App.js
@@ -37,6 +37,14 @@ const parent = React.createElement("div", { id: "parent" }, [
     React.createElement("h2", {}, "I am an H3 tag"),
     React.createElement("h3", {}, "I am an H3 tag")]),
   ),
+
+  React.createElement(
+    "div",
+    { id: "child2" },
+    React.createElement("h4", {}, [React.createElement("h1", {}, "I am an H1 tag"),
+    React.createElement("h2", {}, "I am an H3 tag"),
+    React.createElement("h3", {}, "I am an H3 tag")]),
+  )
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
