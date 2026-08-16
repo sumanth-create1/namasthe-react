6f5d3dba8ba7c09e946ee93b46bd182e4195bb7b
sumanth-create1 <sajjasatyasumanth@gmail.com>
2026-08-16 19:14:29 +0530

    entered in to JSX

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {} , React.createElement("h1" , {} , "Anitha is here"));
+const heading = React.createElement("h1", {id:"heading"}, "Anitha is not here!!");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+
+
+const heading2 = <h1 id = "heading">this is my new REACT</h1>
+
+console.log(heading2);
+
+
+root.render(heading2);
