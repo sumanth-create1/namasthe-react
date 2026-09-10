9b6578639fe6753f4b4a442afd478da1398a19ed
sumanth-create1 <sajjasatyasumanth@gmail.com>
2026-09-10 18:01:39 +0530

    try n to make a good UI

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 const styleCard = {
   backgroundColor: "#F3DAD8",
-}
+};
 
 const Header = () => {
   return (
@@ -30,11 +30,20 @@ const Header = () => {
 const RestaurantCard = () => {
   return (
     <div className="res-card" style={styleCard}>
-      <img className="res-logo" alt="res-logo" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/FOOD_CATALOG/IMAGES/CMS/2026/2/8/046d2492-b606-4a4f-b395-ffe59ded7143_a7c263a5-750c-4d98-a3fa-b45a37471039.JPG" />
-      <h3>Megana Foods</h3>
-      <h4>Biriyani, South Indian, Pan-Asian</h4>
-      <h4>20mins</h4>
-      <h4>4.5 stars</h4>
+      <img
+        className="res-logo"
+        alt="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/FOOD_CATALOG/IMAGES/CMS/2026/2/8/046d2492-b606-4a4f-b395-ffe59ded7143_a7c263a5-750c-4d98-a3fa-b45a37471039.JPG"
+      />
+      <h3 className="res-heading">Megana Foods</h3>
+      <h5 className="res-factors">
+        <ul>
+          <li>4.5</li>
+          <li>25 mins</li>
+        </ul>
+      </h5>
+      <h5>Pizza's</h5>
+      <h5>Ameerpet</h5>
     </div>
   );
 };
@@ -44,6 +53,17 @@ const Body = () => {
     <div className="body">
       <div className="search">search</div>
       <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
         <RestaurantCard />
       </div>
     </div>
